07e11c9d1e6508ff4bdb370d6eee072563c5f0bb
Temur <shernazarovtemurcom001@gmail.com>
2024-02-17 13:52:29 +0500

    footer and hamburger menu are finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,37 +11,46 @@ const Header = () => {
   };
   return (
     <nav>
-      <div className="container nav-items">
-        <div className="left-nav-items">
-          <img src={forkKnife} alt="logo" />
-          <p>eatly</p>
-          <NavLink to="/home">Home</NavLink>
-          <NavLink to="/dishes">Dishes</NavLink>
-        </div>
-        <div className="right-nav-items">
-          <img className="curt" src={curt} alt="curt" />
-          <button className="log">Login</button>
-          <button className="log">Sign Up</button>
-          <div className="mode-border">
-            <img src={darkLight} alt="darkLight" />
+      <div className="container">
+        <div className="nav-items">
+          <div className="left-nav-items">
+            <NavLink to="/home">
+              <img src={forkKnife} alt="logo" />
+            </NavLink>
+            <p>eatly</p>
+            <NavLink className="two-links" to="/home">
+              Home
+            </NavLink>
+            <NavLink className="two-links" to="/dishes">
+              Dishes
+            </NavLink>
           </div>
-          <div className="hamburger-menu">
-            <button
-              className={`menu-btn ${isOpen ? "open" : ""}`}
-              onClick={toggleMenu}
-            >
-              <div className="menu-btn-burger"></div>
-              <div className="menu-btn-burger"></div>
-              <div className="menu-btn-burger"></div>
-            </button>
-            <div className={`menu-items ${isOpen ? "open" : ""}`}>
-              <NavLink to="/home">Home</NavLink>
-              <NavLink to="/dishes">Dishes</NavLink>
-              <NavLink to="/login">Login</NavLink>
-              <NavLink to="/register">Sign Up</NavLink>
+          <div className="right-nav-items">
+            <img className="curt" src={curt} alt="curt" />
+            <button className="log">Login</button>
+            <button className="log">Sign Up</button>
+            <div className="mode-border">
+              <img src={darkLight} alt="darkLight" />
+            </div>
+            <div className="hamburger-menu">
+              <button
+                className={`menu-btn ${isOpen ? "open" : ""}`}
+                onClick={toggleMenu}
+              >
+                <div className="menu-btn-burger"></div>
+                <div className="menu-btn-burger"></div>
+                <div className="menu-btn-burger"></div>
+              </button>
+              <div className={`menu-items ${isOpen ? "open" : ""}`}>
+                <NavLink to="/home">Home</NavLink>
+                <NavLink to="/dishes">Dishes</NavLink>
+                <NavLink to="/login">Login</NavLink>
+                <NavLink to="/register">Sign Up</NavLink>
+              </div>
             </div>
           </div>
         </div>
+        <hr />
       </div>
     </nav>
   );
