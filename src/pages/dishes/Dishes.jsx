541e61a32270ae369145180f3134b422dce91657
Temur <shernazarovtemurcom001@gmail.com>
2024-02-19 21:41:51 +0500

    search fixed

--- src/pages/dishes/Dishes.jsx
+++ src/pages/dishes/Dishes.jsx
@@ -1,185 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import AtomicSpinner from "atomic-spinner";
-// import "./Dishes.scss";
-// import useProduct from "../../App/useProducts";
-
-// import {
-//   isLike,
-//   liked,
-//   chickenHell,
-//   star,
-//   sweDish2,
-//   sweDish3,
-// } from "../../assets/index";
-// import { useNavigate } from "react-router-dom";
-
-// const Dishes = ({ setCarrier }) => {
-//   // LOADING
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [isLoved, setIsLoved] = useState(false);
-//   const [accordionData, setAccordionData] = useState([]);
-//   const { loading, products, error, getProducts, chosenFood, addTodo } =
-//     useProduct();
-//   const [title, setTitle] = useState("");
-//   const [change, setChange] = useState(false);
-//   const navigate = useNavigate();
-
-//   const handlePage = (id) => {
-//     setCarrier(id);
-//     navigate("/details", { replace: true });
-//   };
-
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setIsLoading(false);
-//     }, 2000);
-//   }, []);
-
-//   useEffect(() => {
-//     // Initialize accordion data with questions and content
-//     setAccordionData([
-//       {
-//         isOpen: true,
-//         question: "How long does delivery take?",
-//         content:
-//           "You Can Get Information By Contacting Our Support Team Have 24/7 Service.What’s The Difference Between Free And Paid Plan ?",
-//       },
-
-//       {
-//         question: "How Does It Work ?",
-//         content: "Excellent job",
-//       },
-//       {
-//         question: "How does your food delivery service work?",
-//         content: "It is amazing",
-//       },
-//       {
-//         question: "What payment options do you accept?",
-//         content: "There are a lot of options",
-//       },
-//       {
-//         question: "Do you offer discounts or promotions?",
-//         content: "We have a lot of discounts",
-//       },
-//     ]);
-//   }, []);
-
-//   const handleLoveClick = () => {
-//     setIsLoved(!isLoved);
-//   };
-
-//   const handleAccordionClick = (index) => {
-//     setAccordionData((prevState) => {
-//       const newState = [...prevState];
-//       newState[index].isOpen = !newState[index].isOpen;
-//       return newState;
-//     });
-//   };
-
-//   useEffect(() => {
-//     getProducts();
-//   }, []);
-//   return (
-//     <>
-//       {isLoading ? (
-//         <div className="spinner">
-//           <AtomicSpinner className="spinner" />
-//         </div>
-//       ) : (
-//         <section className="container dishes-hero">
-//           <div className="input-filter">
-//             <div className="search">
-//               <input
-//                 type="search"
-//                 name="search"
-//                 id="search"
-//                 placeholder="Searching..."
-//               />
-//             </div>
-//             <select>
-//               <option value="All">All</option>
-//               <option value="Healthy">Healthy</option>
-//               <option value="Trending">Trending</option>
-//               <option value="Supreme">Supreme</option>
-//             </select>
-//           </div>
-//           <div className="cards">
-//             {/* Render products */}
-//             {products.length > 0 ? (
-//               <div className="cards">
-//                 {products.map((product) => (
-//                   <div className="card" key={product.id}>
-//                     <div className="love-reaction-container">
-//                       <button
-//                         className={`love-reaction-button ${
-//                           isLoved ? "loved" : ""
-//                         }`}
-//                         onClick={handleLoveClick}
-//                       >
-//                         <span className="heart" role="img" aria-label="Love">
-//                           {isLoved ? (
-//                             <img src={liked} alt="img" />
-//                           ) : (
-//                             <img src={isLike} alt="img" />
-//                           )}
-//                         </span>
-//                       </button>
-//                     </div>
-//                     <img
-//                       onClick={() => handlePage(product.id)}
-//                       className="chickenHell"
-//                       src={chickenHell}
-//                       alt="img"
-//                     />
-//                     <p className="type">{product.brand}</p>
-//                     <p onClick={() => handlePage(product.id)}>{product.name}</p>
-//                     <div className="min-rating">
-//                       <p>{product.time} </p>
-//                       <img src={star} alt="img" />
-//                       <p>{product.rating}</p>
-//                     </div>
-//                     <div className="price-add">
-//                       <p>{product.price}</p>
-//                       <button onClick={() => addTodo(product)}>+</button>
-//                     </div>
-//                   </div>
-//                 ))}
-//               </div>
-//             ) : null}
-//           </div>
-//           <hr />
-//           <div className="down-side">
-//             <div className="frequent">
-//               <p>Frequently Asked</p>
-//               <p>Questions</p>
-//             </div>
-//             {accordionData.map(({ question, content, isOpen }, index) => (
-//               <div className="accordion" key={index}>
-//                 <div
-//                   className="show-part-first"
-//                   onClick={() => handleAccordionClick(index)}
-//                 >
-//                   <p>{question}</p>
-//                   <span className={`accordion-arrow ${isOpen ? "open" : ""}`}>
-//                     {isOpen ? "-" : "+"}
-//                   </span>
-//                 </div>
-//                 {isOpen && (
-//                   <div className="get-info">
-//                     <p>{content}</p>
-//                   </div>
-//                 )}
-//               </div>
-//             ))}
-//             <hr />
-//           </div>
-//         </section>
-//       )}
-//     </>
-//   );
-// };
-
-// export default Dishes;
 import React, { useEffect, useState } from "react";
 import AtomicSpinner from "atomic-spinner";
 import "./Dishes.scss";
@@ -203,7 +21,7 @@ const Dishes = ({ setCarrier }) => {
     useProduct();
   const [title, setTitle] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
-  const [filter, setFilter] = useState("All"); // Initialize filter state
+  const [filter, setFilter] = useState("All");
   const navigate = useNavigate();
 
   const handlePage = (id) => {
@@ -218,7 +36,6 @@ const Dishes = ({ setCarrier }) => {
   }, []);
 
   useEffect(() => {
-    // Initialize accordion data with questions and content
     setAccordionData([
       {
         isOpen: true,
@@ -261,7 +78,6 @@ const Dishes = ({ setCarrier }) => {
     });
   };
 
-  // Filter products based on search query and selected filter
   const filteredProducts = products.filter((product) => {
     if (filter !== "All" && product.type !== filter) {
       return false;
@@ -290,7 +106,6 @@ const Dishes = ({ setCarrier }) => {
             </div>
           </div>
           <div className="cards">
-            {/* Render filtered products */}
             {filteredProducts.length > 0 ? (
               <div className="cards">
                 {filteredProducts.map((product) => (
